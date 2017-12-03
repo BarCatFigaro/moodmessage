b98c3539fd5fc1ad4a55a6731e9161901ec3e871
Albert Yip <albertkaiyip@gmail.com>
2017-12-02 16:19:25 -0800

    crawling sorta works

--- main.go
+++ main.go
@@ -1,31 +1,31 @@
 package main
 
-import "fmt"
-import "./twPull"
-import "./stringsem"
-import "github.com/saintpete/twilio-go"
-import "time"
 import (
+	"fmt"
+	"time"
+
 	"github.com/barcatfigaro/moodmessage/search"
+	"github.com/barcatfigaro/moodmessage/stringsem"
+	"github.com/barcatfigaro/moodmessage/twPull"
+	twilio "github.com/saintpete/twilio-go"
 )
 
 func main() {
-    now := time.Now()
+	now := time.Now()
 	spider := search.NewSpider()
-	search.RunSpider(spider, "https://reddit.com/")
+	search.RunSpider(spider, "https://www.reddit.com/r/UBC/")
 
-    messages := []twilio.Message{}
-    newmessages := []string{}
-    for {
-        messages, newmessages = twPull.GetMessages(now,messages)
-        for _,msg := range newmessages {
-            fmt.Println(msg)
-            if stringsem.IsGood(msg){
-                fmt.Println("that was a good message")
-            } else {
-                fmt.Println("that was bad message")
-            }
-        }
-    }
+	messages := []twilio.Message{}
+	newmessages := []string{}
+	for {
+		messages, newmessages = twPull.GetMessages(now, messages)
+		for _, msg := range newmessages {
+			fmt.Println(msg)
+			if stringsem.IsGood(msg) {
+				fmt.Println("that was a good message")
+			} else {
+				fmt.Println("that was bad message")
+			}
+		}
+	}
 }
-
